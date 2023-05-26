82c9cd4c0ca80633ca4109d894fa2a8661785aa3
Shushan04 <ash118817@gmail.com>
2023-05-26 23:09:09 +0400

    add pause button

--- script/index.js
+++ script/index.js
@@ -1,1 +1,16 @@
-const video = document.getElementById("bg-vieo");
+let video = document.getElementById("video");
+
+
+function boo() {
+    let playIcon = document.getElementById("playPause");
+    if (playIcon.classList.contains('fa-play')) {
+        playIcon.classList.remove('fa-play');
+        playIcon.classList.add('fa-pause');
+        video.play();
+    } else {
+        playIcon.classList.remove('fa-pause');
+        playIcon.classList.add('fa-play');
+        video.pause();
+    }
+
+}
